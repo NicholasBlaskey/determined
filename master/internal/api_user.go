4cf1d609e4a43f0893d623878fe6e521457cca06
Nicholas Blaskey <nick.blaskey@hpe.com>
2022-07-28 12:11:46 -0400

    refactor: master.db from db.PgDB to db.DB

--- master/internal/api_user.go
+++ master/internal/api_user.go
@@ -42,7 +42,7 @@ func toProtoUserFromFullUser(user model.FullUser) *userv1.User {
 	}
 }
 
-func getFullModelUser(d *db.PgDB, userID model.UserID) (*model.FullUser, error) {
+func getFullModelUser(d db.DB, userID model.UserID) (*model.FullUser, error) {
 	user, err := d.UserByID(userID)
 	if err == db.ErrNotFound {
 		return nil, errUserNotFound
@@ -50,7 +50,7 @@ func getFullModelUser(d *db.PgDB, userID model.UserID) (*model.FullUser, error) 
 	return user, err
 }
 
-func getUser(d *db.PgDB, userID model.UserID) (*userv1.User, error) {
+func getUser(d db.DB, userID model.UserID) (*userv1.User, error) {
 	user, err := getFullModelUser(d, userID)
 	if err != nil {
 		return nil, err
