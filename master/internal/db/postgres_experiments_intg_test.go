3865e3dc08e1d5071ca7bf237e457878e3968f0c
Nicholas Blaskey <nick.blaskey@hpe.com>
2022-08-17 18:18:41 -0400

    cleanup v2

--- master/internal/db/postgres_experiments_intg_test.go
+++ master/internal/db/postgres_experiments_intg_test.go
@@ -22,7 +22,6 @@ import (
 	"github.com/determined-ai/determined/master/pkg/schemas/expconf"
 	"github.com/determined-ai/determined/proto/pkg/checkpointv1"
 	"github.com/determined-ai/determined/proto/pkg/commonv1"
-	"github.com/determined-ai/determined/proto/pkg/experimentv1"
 	"github.com/determined-ai/determined/proto/pkg/modelv1"
 	"github.com/determined-ai/determined/proto/pkg/trialv1"
 )
