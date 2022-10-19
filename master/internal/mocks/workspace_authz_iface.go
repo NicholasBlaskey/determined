3d226e269f6f17a543c92c394e818f604ad3a1b6
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2022-10-19 18:00:03 -0400

    crud

--- master/internal/mocks/workspace_authz_iface.go
+++ master/internal/mocks/workspace_authz_iface.go
@@ -58,6 +58,20 @@ func (_m *WorkspaceAuthZ) CanCreateWorkspaceWithAgentUserGroup(curUser model.Use
 	return r0
 }
 
+// CanCreateWorkspaceWithCheckpointStorageConfig provides a mock function with given fields: curUser
+func (_m *WorkspaceAuthZ) CanCreateWorkspaceWithCheckpointStorageConfig(curUser model.User) error {
+	ret := _m.Called(curUser)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(model.User) error); ok {
+		r0 = rf(curUser)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // CanDeleteWorkspace provides a mock function with given fields: curUser, _a1
 func (_m *WorkspaceAuthZ) CanDeleteWorkspace(curUser model.User, _a1 *workspacev1.Workspace) error {
 	ret := _m.Called(curUser, _a1)
@@ -109,6 +123,20 @@ func (_m *WorkspaceAuthZ) CanPinWorkspace(curUser model.User, _a1 *workspacev1.W
 
 // CanSetWorkspacesAgentUserGroup provides a mock function with given fields: curUser, _a1
 func (_m *WorkspaceAuthZ) CanSetWorkspacesAgentUserGroup(curUser model.User, _a1 *workspacev1.Workspace) error {
+	ret := _m.Called(curUser, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(model.User, *workspacev1.Workspace) error); ok {
+		r0 = rf(curUser, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// CanSetWorkspacesCheckpointStorageConfig provides a mock function with given fields: curUser, _a1
+func (_m *WorkspaceAuthZ) CanSetWorkspacesCheckpointStorageConfig(curUser model.User, _a1 *workspacev1.Workspace) error {
 	ret := _m.Called(curUser, _a1)
 
 	var r0 error
