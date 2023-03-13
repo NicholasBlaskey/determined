4f434ddb16e9a9746fe7976ca3b10090577e3d5f
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-03-13 17:03:21 -0400

    metrics stream

---
+++ harness/determined/main.py
@@ -0,0 +1,62 @@
+import time
+
+from common import api
+from common.api import authentication, bindings, errors
+
+master_url = "127.0.0.1:8080"
+authentication.cli_auth = authentication.Authentication(master_url)
+sess = api.Session(master_url, None, None, None)
+
+
+def metrics_no_paging(trial_ids):
+    for trial_id in trial_ids:
+        start = time.time()
+        
+        r = bindings.get_MetricsNoPaging(sess, trialId=trial_id)
+        print(f"Trial ID = {trial_id} took {time.time() - start} with # rows = {len(r.steps)}")
+
+print("Metrics no paging")
+metrics_no_paging([1, 2, 3])
+
+def metrics_limit_offset(trial_ids, page_size):
+    for trial_id in trial_ids:
+        def get_with_offset(offset: int) -> bindings.v1MetricsLimitOffsetResponse:
+            return bindings.get_MetricsLimitOffset(
+                sess,
+                offset=offset,
+                limit=page_size,
+                trialId=trial_id,
+            )
+
+        start = time.time()        
+        resps = api.read_paginated(get_with_offset, offset=0, pages="all")
+        r = [s for r in resps for s in r.steps]
+        print(f"Trial ID = {trial_id} took {time.time() - start} with # rows = {len(r)}")
+
+print("\nMetrics limit offset")        
+metrics_limit_offset([1, 2, 3], page_size=250)
+
+def metrics_keyset(trial_ids, page_size):
+    for trial_id in trial_ids:
+        start = time.time()
+        
+        key = ""
+        r = []
+        while True:
+            resp = bindings.get_MetricsKeyset(
+                sess,
+                key=key,
+                size=page_size,
+                trialId=trial_id,
+            )
+            r.extend(resp.steps)
+
+            #print(resp.prevPage, resp.nextPage)
+            key = resp.nextPage
+            if key == "":
+                break
+
+        print(f"Trial ID = {trial_id} took {time.time() - start} with # rows = {len(r)}")
+
+print("\nMetrics keyset")                
+metrics_keyset([1, 2, 3], page_size=250)
