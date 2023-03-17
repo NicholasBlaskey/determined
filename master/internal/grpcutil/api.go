3cca9a70403e6a176fd6311baff8cc813fc16141
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-03-17 09:57:08 -0400

    clean up

--- master/internal/grpcutil/api.go
+++ master/internal/grpcutil/api.go
@@ -112,9 +112,9 @@ func newGRPCGatewayMux() *runtime.ServeMux {
 }
 
 var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
-	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
-	PermitWithoutStream: true,             // send pings even without active streams
+	Time:    10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout: 0,                // wait 1 second for ping ack before considering the connection dead
+	// PermitWithoutStream: true,             // send pings even without active streams
 }
 
 // RegisterHTTPProxy registers grpc-gateway with the master echo server.
